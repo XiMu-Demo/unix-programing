bc26092a1fa7f6d604e4905b1ccc7e25d5f6236e
西木柚子 <nishikiyuzu@ximuyouzideMacBook-Pro.local>
2019-12-05 12:02:13 +0800

    项目初始化

---
+++ unix-programing/unix-programing/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  unix-programing
+//
+//  Created by 西木柚子 on 2019/12/5.
+//  Copyright © 2019 西木柚子. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
